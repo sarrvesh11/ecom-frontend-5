198819a9cd8344f14cef8be6fc7ed8da2f9b4d69
Sarvesh Vajpayee <121345439+sarrvesh11@users.noreply.github.com>
2024-08-11 16:03:28 +0530

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = ({ onSelectCategory, onSearch }) => {
 
   const fetchData = async (value) => {
     try {
-      const response = await axios.get("http://localhost:8080/api/products");
+      const response = await axios.get("http://localhost:8081/api/products");
       setSearchResults(response.data);
       console.log(response.data);
     } catch (error) {
@@ -60,10 +60,10 @@ const Navbar = ({ onSelectCategory, onSearch }) => {
   //       let response;
   //       if (!isNaN(value)) {
   //         // Input is a number, search by ID
-  //         response = await axios.get(`http://localhost:8080/api/products/search?id=${value}`);
+  //         response = await axios.get(`http://localhost:8081/api/products/search?id=${value}`);
   //       } else {
   //         // Input is not a number, search by keyword
-  //         response = await axios.get(`http://localhost:8080/api/products/search?keyword=${value}`);
+  //         response = await axios.get(`http://localhost:8081/api/products/search?keyword=${value}`);
   //       }
 
   //       const results = response.data;
